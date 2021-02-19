ca37cbed42dd2a8bf142f32daa634205d0256632
aj-ward <59932988+rj-ward@users.noreply.github.com>
2021-02-19 15:21:23 -0600

    Add watcher for message reactions

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const prefix = '&';
 
 client.on('ready', () => {
@@ -118,4 +118,25 @@ client.on('message', (msg) => {
     }
     
   }
-})
+
+
+}
+)
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
