f7e6258393510a306c36181737f837a21c32e718
aj-ward <59932988+rj-ward@users.noreply.github.com>
2021-02-19 14:38:39 -0600

    add kick command to kick users from server

--- bot.js
+++ bot.js
@@ -35,29 +35,29 @@ client.on('message', async (msg) => {
         msg.reply('wow, what a great post')
       }
     
-      if (command === "clear") {
+    if (command === "clear") {
         //default deletes message itself plus previous
         let num = 2;
         
         //if argument is provided, convert it from string to number
         if (args[0]) {
-          //add 1 to delete clear command itself
-          num = parseInt(args[0]) + 1;
+            //add 1 to delete clear command itself
+            num = parseInt(args[0]) + 1;
         }
         //bulk delete the messages
         msg.channel.bulkDelete(num);
         //notify channel of deleted messages
         msg.channel.send(`deleted  ${args[0]} posts for you`);
-      }
+    }
 
-      if(command === 'joke') {
+    if(command === 'joke') {
         //async API call using async/await syntax
         let getJoke = async () => {
-          //make API call
-          let result = await fetch('https://official-joke-api.appspot.com/random_joke')
-          //convert to object we can work with
-          let json = await result.json()
-          return json
+            //make API call
+            let result = await fetch('https://official-joke-api.appspot.com/random_joke')
+            //convert to object we can work with
+            let json = await result.json()
+            return json
         }
         //call function defined above
         let joke = await getJoke()
@@ -69,4 +69,28 @@ client.on('message', async (msg) => {
         ${joke.punchline}
         `)
       }
+      
+      if(command === 'kick') {
+        //verify that user has moderation role
+        if(!msg.member.roles.cache.has('812421021830348810')) {
+          msg.reply('you dont have permission to kick users')
+          // if user doesnt have the role, we return without kicking the user
+          return
+        }
+        //check to make sure a user was actually mentioned, if not we return because bot doesnt know who to kick
+        const user = msg.mentions.users.first()
+        if(!user) {
+          msg.reply('no user mentioned')
+          return
+        }
+        //if user was mentioned, grab their guild member information
+        const member = msg.guild.member(user)
+        //if they are a member of the server, kick them
+        if(member) {
+          member.kick('this is a message for the server logs').then(() => {
+            msg.reply(`${user.tag} was kicked from the server`)
+          })
+        }
+      }
+      
 })
