c96c7f923458400c8e19aa9b9fe4a4d1caaaeddf
aj-ward <59932988+rj-ward@users.noreply.github.com>
2021-02-19 14:18:34 -0600

    add joke API call command

--- bot.js
+++ bot.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
 const client = new Discord.Client();
-const prefix = '&'
+const prefix = '&';
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -46,5 +48,25 @@ client.on('message', async (msg) => {
         msg.channel.bulkDelete(num);
         //notify channel of deleted messages
         msg.channel.send(`deleted  ${args[0]} posts for you`);
+      }
+
+      if(command === 'joke') {
+        //async API call using async/await syntax
+        let getJoke = async () => {
+          //make API call
+          let result = await fetch('https://official-joke-api.appspot.com/random_joke')
+          //convert to object we can work with
+          let json = await result.json()
+          return json
+        }
+        //call function defined above
+        let joke = await getJoke()
+        
+        //have our bot reply using the data returned from our API call
+        msg.reply(`
+        Here's your joke
+        ${joke.setup}
+        ${joke.punchline}
+        `)
       }
 })
