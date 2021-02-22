777e00a53a03aca5d41a2413443d4be8b492f467
aj-ward <59932988+rj-ward@users.noreply.github.com>
2021-02-22 10:09:26 -0600

    diagnosing error in roll command

--- bot.js
+++ bot.js
@@ -107,7 +107,8 @@ client.on('message', async(msg) => {
     if(command === 'roll') {
         let size = 6
         console.log(Number.isInteger(Number(args)));
-        if (Number.isInteger(Number(args)) && !'' && args === args) {
+        console.log(Number(args) === Number(args));
+        if (Number.isInteger(Number(args)) && !'') {
             size = parseInt(msg.content.toLowerCase().substring(6));
         };
         result = roll(size);
